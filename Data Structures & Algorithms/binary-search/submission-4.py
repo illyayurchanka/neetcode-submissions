f655a1435db04e259edc75a9544b640d28e2f54c
KUWE <36008372+illyayurchanka@users.noreply.github.com>
2026-09-23 14:15:20 +0200

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def med_point(l, r):
+            return l + (r - l) // 2
+        
+        i = 0
+        j = len(nums) - 1
+        med = med_point(i, j)
+        if nums[med] == target:
+            return med
+        count = 0
+        while i <= j and count < 10:
+            if nums[med] > target:
+                j = med - 1
+                med = med_point(i, j)
+            elif nums[med] < target:
+                i = med + 1
+                med = med_point(i, j)
+            else:
+                return med
+            # count += 1
+        return -1
